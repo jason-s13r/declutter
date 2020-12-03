1a81bc139f6e5f2fcb021ff47921e9c47eb3f6da
Jason Schwarzenberger <jason@credisense.io>
2020-12-04 11:41:00 +1300

    add more metadata parsing.

--- utils/extract-metadata.js
+++ utils/extract-metadata.js
@@ -1,85 +1,52 @@
 const { JSDOM } = require("jsdom");
 const { Readability } = require("@mozilla/readability");
+const metascraper = require('metascraper')([
+	require('metascraper-author')(),
+	require('metascraper-date')(),
+	require('metascraper-description')(),
+	require('metascraper-image')(),
+	require('metascraper-logo')(),
+	require('metascraper-publisher')(),
+	require('metascraper-title')(),
+	require('metascraper-url')(),
+	require('metascraper-readability')()
+]);
 // const { parse } = require("@postlight/mercury-parser");
 
 module.exports.extractReadable = extractReadable;
 module.exports.extractMetadata = extractMetadata;
-module.exports.cleanHtmlText = cleanHtmlText;
-module.exports.addMetadata = addMetadata;
 
 async function extractReadable(html, url) {
 	const doc = new JSDOM(html, { url });
 	const reader = new Readability(doc.window.document);
 	let readable = reader.parse();
-	let { author, publisher, authorType } = extractMetadata(html, url)
 
+	readable.textContent = undefined;
+	readable.dir = undefined;
 	readable.url = url;
-	readable = addMetadata(readable, authorType, author, publisher, url);
+	readable.meta = await extractMetadata(html, url);
+
+	readable.author = readable.meta.author;
+	readable.publisher = readable.meta.publisher;
+	readable.byline = [readable.meta.author, readable.meta.publisher].filter((s) => !!s && !!s.trim()).join(" &bull; ");
+
 	return readable;
 }
 
-function extractMetadata(html, url) {
+async function extractMetadata(html, url) {
 	const dom = new JSDOM(html, { url });
 	const document = dom.window.document;
 
-	const meta = {
-		author: "",
-		publisher: new URL(url).host,
-		authorType: "",
-	};
+	const metadata = await metascraper({ html, url });
+	metadata.og = extractOpengraph(document);
 
-	const ogProps = document.querySelector('meta[property="og:site_name"]');
-	const itemProps = document.querySelectorAll('[itemprop="author"]');
-	const ldJsonTags = document.querySelectorAll(
-		'script[type="application/ld+json"]'
-	);
-
-	if (ogProps) {
-		meta.author = ogProps && ogProps.content ? ogProps.content : "";
-		meta.authorType = "og";
-	}
-
-	Array.from(itemProps).forEach((element) => {
-		meta.publisher = meta.author;
-		meta.author = element.innerText;
-		meta.authorType = "ld";
-	});
-
-	Array.from(ldJsonTags).forEach((ldTag) => {
-		try {
-			const ld = JSON.parse(ldTag.innerHTML);
-			if (ld["@type"] === "Article") {
-				if (ld.author && ld.author["@type"] === "Person") {
-					meta.author = ld.author.name;
-					meta.authorType = "ld";
-				}
-				if (ld.publisher && ld.publisher["@type"] === "Organization") {
-					meta.publisher = ld.publisher.name;
-				}
-			}
-		} catch (e) { }
-	});
-	return meta;
+	return metadata;
 };
 
-function cleanHtmlText(text) {
-	const s = new JSDOM("").window.document.createElement("span");
-	s.innerHTML = text;
-	return s.textContent;
-};
-
-function addMetadata(readable, authorType, author, publisher) {
-	if (authorType !== "ld") {
-		publisher = author;
-		author = readable.author || readable.byline;
-	}
-
-	const authorName = cleanHtmlText(
-		[author, publisher].filter((s) => !!s && !!s.trim()).join(" &bull; ")
-	);
-
-	readable.byline = authorName;
-	readable.author = author;
-	readable.publisher = publisher;
-	return readable;
+function extractOpengraph(document) {
+	const props = document.querySelectorAll('meta[property^="og:"]');
+	return Array.from(props).reduce((meta, prop) => ({
+		...meta,
+		[prop.attributes.getNamedItem('property').value]: prop.attributes.getNamedItem('content').value
+	}), {})
 }
