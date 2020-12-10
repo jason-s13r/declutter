acdb3e68953dd07979fb1e6185a916a9253add3c
Jason Schwarzenberger <jason@credisense.io>
2020-12-10 16:14:38 +1300

    video support.

--- utils/extract-metadata.js
+++ utils/extract-metadata.js
@@ -9,49 +9,102 @@ const metascraper = require('metascraper')([
 	require('metascraper-publisher')(),
 	require('metascraper-title')(),
 	require('metascraper-url')(),
-	require('metascraper-readability')()
+	require('metascraper-readability')(),
+	require('metascraper-media-provider')()
 ]);
+const youtubedl = require('youtube-dl');
 const Turndown = require('turndown');
-const { Converter } = require('showdown');
-// const { parse } = require("@postlight/mercury-parser");
+const createDOMPurify = require('dompurify');
 
 module.exports.extractReadable = extractReadable;
 module.exports.extractMetadata = extractMetadata;
 
 async function extractReadable(html, url) {
-	const doc = new JSDOM(html, { url });
-	const reader = new Readability(doc.window.document);
+	const metadata = await extractMetadata(html, url);
+	const { window } = new JSDOM(html, { url });
+
+	if (metadata.videos) {
+		replaceVideos(window, metadata.videos);
+	}
+
+	const reader = new Readability(window.document);
 	let readable = reader.parse();
 
 	readable.textContent = undefined;
 	readable.dir = undefined;
 	readable.url = url;
-	readable.meta = await extractMetadata(html, url);
+	readable.meta = metadata;
 
 	readable.author = readable.meta.author;
 	readable.publisher = readable.meta.publisher;
 
-	const byline = doc.window.document.createElement('span');
+	const byline = window.document.createElement('span');
 	byline.innerHTML = [readable.meta.author, readable.meta.publisher].filter((s) => !!s && !!s.trim()).join(" &bull; ");
 	readable.byline = byline.textContent;
 
-	readable.html = readable.content;
-	readable.markdown = new Turndown().turndown(readable.content);
-	readable.content = new Converter().makeHtml(readable.markdown);
+	const DOMPurify = createDOMPurify(new JSDOM('').window);
 
+	readable.html = readable.content;
+	readable.content = DOMPurify.sanitize(readable.html);
+	readable.markdown = new Turndown().turndown(readable.content);
 
 	return readable;
 }
 
 async function extractMetadata(html, url) {
-	const dom = new JSDOM(html, { url });
-	const document = dom.window.document;
+	const { window } = new JSDOM(html, { url });
 
 	const metadata = await metascraper({ html, url });
-	metadata.og = extractOpengraph(document);
+	metadata.og = extractOpengraph(window.document);
+	try {
+		console.log('doing a youtube-dl');
+		metadata.videos = await extractVideos(url);
+	} catch (e) {
+		console.error(e);
+	}
 
 	return metadata;
 };
+
+async function extractVideos(url) {
+	return new Promise((resolve, reject) => {
+		const args = ['-j'];
+		const opts = {};
+		youtubedl.getInfo(url, args, opts, (e, o) => {
+			if (e) {
+				reject(e);
+			}
+			resolve(o instanceof Array ? o : [o]);
+		});
+	});
+}
+
+function replaceVideos(window, videos) {
+	if (!videos) {
+		return;
+	}
+	videos.forEach(info => {
+		const id = info.id.replace(/(^ref\:)/, '');
+		console.log(info.info, id);
+		const $original = window.document.querySelector(`[data-video-id="${info.id}"],[data-video-id="${id}"],[id*="${info.id}"],[id*="${id}"]`)
+		if (!$original) {
+			console.log('no video', info.info, id);
+			return;
+		}
+		const $parent = $original.parentNode;
+		const $fixed = window.document.createElement('figure');
+		const $video = window.document.createElement('video');
+		const $caption = window.document.createElement('figcaption');
+		$video.src = info.url;
+		$video.setAttribute('controls', 'controls');
+		$video.setAttribute('title', info.title);
+		$caption.textContent = info.description;
+		$fixed.appendChild($video);
+		$fixed.appendChild($caption);
+		$parent.insertBefore($fixed, $original);
+		$original.remove();
+	});
+}
 
 function extractOpengraph(document) {
 	const props = document.querySelectorAll('meta[property^="og:"]');
