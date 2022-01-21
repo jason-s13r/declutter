d6575bf33342214f4f2f54b08597f44adb224222
Jason Schwarzenberger <jason@master5o1.com>
2022-01-21 22:50:44 +1300

    cache

--- routes/headless/get-link-codes.js
+++ routes/headless/get-link-codes.js
@@ -7,11 +7,21 @@ module.exports = {
 
 async function getLinkCodes(req, res) {
   const url = req.body.url;
+	const nocache = !!req.body.nocache;
   if (!/https?:\/\/(www\.)?.*\/.*/i.test(url)) {
     return res.status(400);
   }
-  console.log("[headless/link-codes] vending machine goes brrr...");
-  const machine = await scraper.getLinkCodes(url);
+  console.log(`[headless/link-codes] for url ${url}`);
+  let machine = cache.linkCodes.get(url);
+  if (nocache) {
+    machine = undefined;
+    console.log("[headless/link-codes] no cache...");
+  }
+  if (!machine) {
+    console.log("[headless/link-codes] vending machine goes brrr...");
+    machine = await scraper.getLinkCodes(url);
+    cache.linkCodes.set(url, machine);
+  }
   if (!machine) {
     return res.status(500);
   }
--- utils/cache.js
+++ utils/cache.js
@@ -6,9 +6,11 @@ const HOUR = 60 * MINUTE;
 const telegraph = new NodeCache({ stdTTL: 24 * HOUR });
 const declutter = new NodeCache({ stdTTL: 4 * HOUR });
 const comment = new NodeCache({ stdTTL: 30 * MINUTE });
+const linkCodes = new NodeCache({ stdTTL: 30 * MINUTE });
 
 module.exports = {
 	telegraph,
 	declutter,
-	comment
+	comment,
+	linkCodes
 };
